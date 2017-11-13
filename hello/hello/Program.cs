357d6c9ba019c171ba9eabc851ee55cbb8750fb8
TomaszStarek <33632173+TomaszStarek@users.noreply.github.com>
2017-11-13 20:08:54 +0100

    Add files via upload

---
+++ hello/hello/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace hello
+{
+    public class Program
+    {
+         static void Main()
+        {
+            
+            Console.WriteLine("Dzem dobry!");
+
+           
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            /*        
+                  Console.WriteLine("Podaj dane: ");
+                  string liczba = Console.ReadLine();
+                  Console.WriteLine(liczba);
+                  int maska = 0;
+                  int a=0, b = 0, suma=0;
+                  
+                  int x = 0;
+      
+                  Int32.TryParse(liczba, out x);
+      
+      
+                  
+                  
+             /*     for (int i = 1; i < liczba; i++)
+                  {
+                      maska = 1 << (i - 1);
+                      b = (x & maska) >> (i - 1);
+                      a ^= b;
+                    
+          
+      
+                  }
+                  Console.WriteLine("suma: ");
+            Console.WriteLine(a);
+
+            //Console.ReadLine();
+            */
+            Console.WriteLine("Press any key to exit.");
+            
+            
+        }
+    }
+}
